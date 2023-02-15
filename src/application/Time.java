8c0c7da6b235522f1dca713928171dd4190e098c
Arnav Bhalla <71564120+arnavbhalla-byte@users.noreply.github.com>
2023-02-15 16:45:51 -0700

    Update Time.java

--- src/application/Time.java
+++ src/application/Time.java
@@ -8,17 +8,5 @@ public static String convertToTime(int digits, boolean showLocalTime) {
     }
     int hours = digits / 100;
     int minutes = digits % 100;
-    if (showLocalTime) {
-        Calendar calendar = Calendar.getInstance();
-        hours += calendar.get(Calendar.HOUR_OF_DAY);
-        minutes += calendar.get(Calendar.MINUTE);
-        if (minutes >= 60) {
-            hours += 1;
-            minutes -= 60;
-        }
-        if (hours >= 24) {
-            hours -= 24;
-        }
-    }
     return String.format("%02d:%02d", hours, minutes);
 }
