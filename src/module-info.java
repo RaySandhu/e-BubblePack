ad85f20d0e9d63114e1b3d4d70c2cf6ff7bf2a4b
RaySandhu <aryaman.sandhu@ucalgary.ca>
2023-03-01 21:33:40 +0000

    Responsive Navbar design with no styling or functionality yet

--- src/module-info.java
+++ src/module-info.java
@@ -2,4 +2,7 @@ module EBubblePack {
 	// requires javafx.controls;
 	
 	opens application to javafx.graphics, javafx.fxml;
+	requires javafx.graphics;
+	requires javafx.fxml;
+	requires javafx.controls;
 }
