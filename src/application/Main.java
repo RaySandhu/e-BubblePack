b5ce6b2d4dcc681a0165f59b6363522c6b000ecb
RaySandhu <aryaman.sandhu@ucalgary.ca>
2023-03-07 20:55:06 +0000

    Converted the mock display for medication to java in controller.
    This allows arbitrary instantiation as I could make sense of it. Each
    dosage of each medication also is iterated through to display each
    dosage and not just one button per medication. Next I will display these
    in chronological order, display colors indicating taken, missed, or not
    due, and a method to toggle through those.

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,6 +12,15 @@ import javafx.scene.layout.*;
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
+		
+		MedList.addMedications("Tylenol3 - mock", 500,"g", "024", "0800,1200,1600,2000");
+		MedList.addMedications("Pantoprazole - mock", 200,"mg", "0123456", "0600,1000");
+		MedList.addMedications("Ibuprofen - mock", 5,"ml", "5", "1800");
+		MedList.addMedications("Dexamethasone - mock", 400, "g", "246", "0901,1312,2023,2355");
+		MedList.addMedications("Maxeran - mock", 200, "g", "0135", "0610,1050,");
+		MedList.addMedications("Zofran - mock", 700, "mg", "1356", "1900,2222,2323,2400");
+		MedList.addMedications("Propofol - mock", 1000, "g", "235", "0000,1000,1400,1800");
+		MedList.addMedications("Ketamine - mock", 25,"g", "145", "0600,1000,1400,2400");
 		try {
 			FXMLLoader loader = new FXMLLoader();
 
@@ -24,19 +33,4 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
-
-	//	public static void main(String[] args) {
-	//		// mock medications on program run
-	//		MedList.addMedications("Tylenol3 - mock", 500,"g", "024", "0800,1200,1600,2000");
-	//		MedList.addMedications("Pantoprazole - mock", 200,"mg", "0123456", "0600,1000");
-	//		MedList.addMedications("Ibuprofen - mock", 5,"ml", "5", "1800");
-	//		MedList.addMedications("Dexamethasone - mock", 400, "g", "246", "0901,1312,2023,2355");
-	//		MedList.addMedications("Maxeran - mock", 200, "g", "0135", "0610,1050,");
-	//		MedList.addMedications("Zofran - mock", 700, "mg", "1356", "1900,2222,2323,2400");
-	//		MedList.addMedications("Propofol - mock", 1000, "g", "235", "0000,1000,1400,1800");
-	//		MedList.addMedications("Ketamine - mock", 25,"g", "145", "0600,1000,1400,2400");
-	//
-	//		Display.introScene();
-	//		launch(args);
-	//	}
 }
--- src/application/MainDisplayPaneController.java
+++ src/application/MainDisplayPaneController.java
@@ -5,14 +5,20 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 import javafx.util.Duration;
+
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.*;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
 
 public class MainDisplayPaneController {
 
@@ -23,8 +29,49 @@ public class MainDisplayPaneController {
     @FXML
     private Label medNameDisplay;
 
-    @FXML
-    private Button medDisplayButton;
+    private Button medDisplayButton(String medName, String dosage, int singleDoseTime) {
+    	Button medDisplayButton = new Button();
+    	medDisplayButton.setPrefSize(700, 75);
+    	medDisplayButton.setFont(new Font(18));
+
+    	HBox hbox = new HBox();
+    	hbox.setAlignment(Pos.CENTER_LEFT);
+    	hbox.setPrefSize(800, 75);
+
+    	Label medNameDisplay = new Label(medName);
+    	medNameDisplay.setPrefWidth(150);
+    	medNameDisplay.setFont(new Font(18));
+    	medNameDisplay.setWrapText(true);
+    	HBox.setMargin(medNameDisplay, new Insets(0, 0, 0, 15));
+
+    	Label medDosageDisplay = new Label(dosage);
+    	medDosageDisplay.setPrefWidth(100);
+    	medDosageDisplay.setFont(new Font(18));
+    	HBox.setMargin(medDosageDisplay, new Insets(0, 0, 0, 10));
+
+    	Pane spacer = new Pane();
+    	spacer.setPrefSize(200, 75);
+
+    	Label medDoseTimeDisplay = new Label("" + singleDoseTime);
+    	medDoseTimeDisplay.setPrefWidth(100);
+    	medDoseTimeDisplay.setFont(new Font(18));
+    	HBox.setMargin(medDoseTimeDisplay, new Insets(0, 10, 0, 0));
+
+    	MenuButton menuButton = new MenuButton();
+    	menuButton.setText("");
+    	menuButton.setTextAlignment(TextAlignment.CENTER);
+    	menuButton.setPrefWidth(0);
+
+    	MenuItem action1 = new MenuItem("Action 1");
+    	MenuItem action2 = new MenuItem("Action 2");
+    	menuButton.getItems().addAll(action1, action2);
+
+    	hbox.getChildren().addAll(medNameDisplay, medDosageDisplay, spacer, medDoseTimeDisplay, menuButton);
+
+    	medDisplayButton.setGraphic(hbox);
+    	return medDisplayButton;
+
+    };
 
     @FXML
     private Label medDosageDisplay;
@@ -34,6 +81,7 @@ public class MainDisplayPaneController {
     
     @FXML
     private VBox dailyMedsDisplay;
+    
     
     public void getSelectedDay(ActionEvent e) {
     	String selectedDayValue = ((Button)e.getSource()).getText();
@@ -66,6 +114,29 @@ public class MainDisplayPaneController {
     
     public void renderMedList(int selectedDay) {
     	System.out.print(selectedDay);
+    	ArrayList<Medication> listToRender = Display.dailyMedicationList(selectedDay);
+		dailyMedsDisplay.getChildren().clear();
+    	for (Medication i : listToRender) {
+    		System.out.println(i.getTradeName());
+    		i.updateMissedMeds();
+    		ArrayList<Integer> timesDue = i.getSchedule().get(1);
+
+    		for(int j=0; j<timesDue.size(); j++) {
+    			Boolean adminStatus = i.getAdministrationRecord().get(j).get(0);
+    			Boolean missedStatus = i.getAdministrationRecord().get(j).get(1);
+    			String displayStatus = "";
+
+    			if(adminStatus) {
+    				displayStatus = "Taken";
+    			} else if (missedStatus) {
+    				displayStatus = "Missed";
+    			} else if(!adminStatus && !missedStatus) {
+    				displayStatus = "Not due yet";
+    			}
+ 
+        		dailyMedsDisplay.getChildren().add(medDisplayButton(i.getTradeName(),i.getDosage(), timesDue.get(j)));
+    		}
+    	}
     }
 
 	@FXML
